1a4abb5ab34b739d9cdf91e64b264e5d4701f477
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-10-22 21:49:55 +0100

    More non existent functionality

--- spec/controllers/user_settings_controller_spec.rb
+++ spec/controllers/user_settings_controller_spec.rb
@@ -4,6 +4,7 @@ describe UserSettingsController do
 
   describe "GET 'change_manager'" do
     it "should be successful" do
+      pending "No the functionality implemented"
       get 'change_manager'
       response.should be_success
     end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'manage_user'" do
-    it "should be successful" do
-      get 'manage_user'
-      response.should be_success
-    end
-  end
-
 end
