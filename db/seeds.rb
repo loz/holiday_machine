556e2507fc3c88e7da44207d89d5abb2caea3e81
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-10-23 08:28:55 +0100

    Added seeds users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-HolidayStatus.create([{:status => 'Pending'}, {:status => 'Authorised'}, {:status => 'Cancelled'}, {:status => 'Taken'}])
-UserType.create([{:name=>"Standard"}, {:name=>"Manager"}])
+HolidayStatus.create!([{:status => 'Pending'}, {:status => 'Authorised'}, {:status => 'Cancelled'}, {:status => 'Taken'}])
+UserType.create!([{:name=>"Standard"}, {:name=>"Manager"}])
 
 #2011 holidays
-BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2011-01-03'},
+BankHoliday.create!([{:name=>'New Years Day', :date_of_hol=>'2011-01-03'},
                     {:name=>'Good Friday', :date_of_hol=>'2011-04-22'},
                     {:name=>'Easter Monday', :date_of_hol=>'2011-04-25'},
                     {:name=>'Royal Wedding', :date_of_hol=>'2011-04-29'},
@@ -13,7 +13,7 @@ BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2011-01-03'},
                     {:name=>'Boxing Day', :date_of_hol=>'2011-12-27'}])
 
 #2012 holidays
-BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2012-01-02'},
+BankHoliday.create!([{:name=>'New Years Day', :date_of_hol=>'2012-01-02'},
                     {:name=>'Good Friday', :date_of_hol=>'2012-04-06'},
                     {:name=>'Easter Monday', :date_of_hol=>'2012-04-09'},
                     {:name=>'Early May Bank Holiday', :date_of_hol=>'2012-05-07'},
@@ -23,6 +23,24 @@ BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2012-01-02'},
                     {:name=>'Christmas Day', :date_of_hol=>'2012-12-25'},
                     {:name=>'Boxing Day', :date_of_hol=>'2012-12-26'}])
 
-HolidayYear.create([{:description => "Oct 2010 to Sept 2011", :date_start => "2010-10-01", :date_end=> "2011-09-30"},
+HolidayYear.create!([{:description => "Oct 2010 to Sept 2011", :date_start => "2010-10-01", :date_end=> "2011-09-30"},
                     {:description => "Oct 2011 to Sept 2012", :date_start => "2011-10-01", :date_end=> "2012-09-30"},
                     {:description => "Oct 2012 to Sept 2013", :date_start => "2012-10-01", :date_end=> "2013-09-30"}])
+
+User.new do |u|
+  u.email = "admin@test.com"
+  u.password = "password"
+  u.confirmed_at = Time.now
+  u.forename = "Admin"
+  u.surname = "User"
+  u.user_type = UserType.find(2)
+end.save :validate => false
+
+User.new do |u|
+  u.email = "regular@test.com"
+  u.password = "password"
+  u.confirmed_at = Time.now
+  u.forename = "Regular"
+  u.surname = "User"
+  u.user_type = UserType.find(1)
+end.save :validate => false
